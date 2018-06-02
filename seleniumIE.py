03b2c643a6413bb50bcead50b8fb94de8cf0c802
Saad <sfp300@hotmail.com>
2018-06-02 16:24:31 -0500

    final changes

--- seleniumIE.py
+++ seleniumIE.py
@@ -6,12 +6,14 @@ Created on Apr 6, 2017
 import time
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
-#iepath =  "C:\saadEnvy\workspace\IEDriverServer64v3.exe"
+
+
 iepath = "IEDriverServer64v3.exe"
 url = "https://rubygems.org/"
 rubyInputXpath = "//*[@id='home_query']"
 rubyfirstResponse= "(//h2[contains(.,'ruby-debug19')])[1]"
 authorsXpath = "/html/body/main/div/div/div[1]/div[4]/ul/li/p"
+
 driver = webdriver.Ie(iepath)
 driver.get(url = "https://rubygems.org/")
 print(driver.title)
@@ -22,9 +24,12 @@ driver.find_element_by_xpath(rubyInputXpath).send_keys("ruby-debug19")
 driver.find_element_by_xpath(rubyInputXpath).send_keys(Keys.ENTER)
  
 driver.find_element_by_xpath(rubyfirstResponse).click()
-time.sleep(2)
-print(driver.find_elements_by_xpath(authorsXpath)[0].text)
-# for elem in driver.find_elements_by_xpath(authorsXpath):
-#     print (elem.text)
-#print(driver.find_element_by_xpath(authorsXpath).text)
+
+
+
+for elem in driver.find_elements_by_xpath("//div[@class='dependencies']//a[@class='t-list__item']"):
+    print(elem.text)
+
+print(driver.find_elements_by_xpath(authorsXpath).text)
+ 
 driver.quit()
